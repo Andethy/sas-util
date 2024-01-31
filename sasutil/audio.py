9dea102f12db124719f469e0f310e59d913af72c
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2024-01-31 16:16:06 -0500

    Created annotations formatted json functions

--- sasutil/audio.py
+++ sasutil/audio.py
@@ -1,12 +1,13 @@
 import os.path
+import time
 from pathlib import Path
 
-from file import FileFinder
+from constants import TAGS
+from file import FileFinder, JsonFileIO, CsvFileIO
 from pydub import AudioSegment
-
+from queue import Queue
 
 class AudioFormatter:
-
     finder: FileFinder
     location: Path
     trim: float  # length to trim files at
@@ -27,8 +28,6 @@ class AudioFormatter:
                 self._format_file(path)
 
     def _format_file(self, file: Path):
-        # rate, audio = wav_read(file)
-        # audio = audio
         segment = ...
         if file.suffix == '.wav':
             segment = AudioSegment.from_wav(file)
@@ -40,10 +39,38 @@ class AudioFormatter:
         rel_path = self.finder.get_rel_path(file)
         exp_path = Path(os.path.abspath('../out/' + str(rel_path)))
         exp_path.mkdir(parents=True, exist_ok=True)
-        segment[:1000 * self.trim].export(exp_path.joinpath(file.name))
+        segment[:int(1000 * self.trim)].export(exp_path.joinpath(file.name))
         print("Exported", file.name, "to", rel_path)
 
 
+class AudioOrganizer:
+
+    def __init__(self, in_dir: str):
+        self.finder = FileFinder(in_dir, {'wav', 'mp3', 'ogg'})
+        self.csv_file = CsvFileIO('../resources/tagatune/annotations.csv')
+        self.json_file = JsonFileIO('../resources/tagatune/annotations.json')
+
+    def init_json(self):
+        res = Queue()
+        print(f"Attempting to add tracks to buffer")
+        while res.qsize() < self.csv_file.entries:
+            curr = self.csv_file.get_next()
+
+            if not curr:
+                break
+            sub = []
+            for tag, value in zip(TAGS[1:-1], curr[1:-1]):
+                if value == '1':
+                    sub.append(tag)
+            res.put((curr[0], sub, curr[-1]))
+
+        print("Attempting to encode to JSON")
+        self.json_file.add_entries(res)
+        print("Encoded to JSON - DONE")
+
+
 if __name__ == '__main__':
-    formatter = AudioFormatter(os.path.abspath('../resources'))
-    formatter.format_all()
+    organizer = AudioOrganizer('../resources/data')
+    organizer.init_json()
+    # formatter = AudioFormatter(os.path.abspath('../resources'))
+    # formatter.format_all()
--- sasutil/file.py
+++ sasutil/file.py
@@ -1,6 +1,10 @@
+import json
 import os
 from pathlib import Path
 from queue import Queue
+from typing import TextIO
+
+from constants import JSON_FIELDS
 
 
 class FileFinder(object):
@@ -31,3 +35,97 @@ class FileFinder(object):
 
     def get_rel_path(self, file: Path):
         return Path(os.path.relpath(file.parent, self.root))
+
+    def get_full_path(self, file: Path):
+        return self.root.joinpath(file)
+
+
+class BaseFileIO:
+    file: TextIO
+    file_path: str
+    data: list
+
+    def __init__(self, file_path='', file=...):
+        self.file = file
+        self.file_path = file_path
+        self.data = []
+
+        if file_path:
+            try:
+                self.set_file(self.file_path)
+            except IOError:
+                pass
+
+    def set_file(self, file_path):
+        self.file_path = str(file_path)
+        self.file = open(str(self.file_path))
+
+    def manipulate_file(self, data):
+        self.file.close()
+        self.file = open(self.file_path, 'w')
+        self.file.writelines(data)
+        self.file.close()
+
+    def get_data(self):
+        return self.data
+
+    def get_text(self):
+        try:
+            return self.file.read().splitlines()
+        except ValueError:
+            self.file = open(self.file_path, 'r')
+            return self.file.read().splitlines()
+
+
+class CsvFileIO(BaseFileIO):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.entries = len(self.file.readlines()) - 1
+        self.file.seek(0)
+        self.headers = self.get_next()
+
+    def get_next(self):
+        row = self.file.readline()[:-2].replace('\t', '').split('""')
+        row[0] = row[0].replace('"', '')
+        return row
+
+
+class JsonFileIO(BaseFileIO):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def add_entries(self, entries: Queue):
+
+        with open(self.file_path, 'r', encoding="utf-8", errors="replace") as self.file:
+            self.data = json.load(self.file)
+
+        for n in range(entries.qsize()):
+            entry = {}
+            items = entries.get()
+            for index, key in enumerate(JSON_FIELDS):
+                entry[key] = items[index]
+            self.data.append(entry)
+
+        self.file = open(self.file_path, 'w', encoding="utf-8", errors="replace")
+        self.file.write(json.dumps(self.data, sort_keys=False, indent=4))
+        self.file.close()
+
+    def clear_entries(self):
+        with open(self.file_path, 'w', encoding="utf-8", errors="replace") as self.file:
+            self.file.write(json.dumps([], sort_keys=False, indent=4))
+
+    def get_entries(self):
+        try:
+            with open(self.file_path, 'r') as self.file:
+                self.data = json.load(self.file)
+                print("DATA:", self.data)
+        except FileNotFoundError:
+            print("NOT FOUND?")
+        print(self.data)
+        return self.data
+
+
+if __name__ == '__main__':
+    csv = CsvFileIO('../resources/tagatune/annotations.csv')
+    print(str(csv.headers).replace(',', ',\n'))
