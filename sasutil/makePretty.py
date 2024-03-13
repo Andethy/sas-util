096bfc8346291bac69ea922e7324a2d9d9baa9c8
colby brown <colbyironman14@gmail.com>
2024-03-13 19:26:18 -0400

    adding makePretty.py adds boxplot support

--- sasutil/makePretty.py
+++ sasutil/makePretty.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 #EVALUATION_KEYS = ('Danger', 'Urgency', 'Risk of Failure', 'Collaboration', 'Approachable')
 def convert(values):
     trackMeans = {}
@@ -11,3 +13,25 @@ def convert(values):
             personTracks = np.vstack(personTracks, trackData)
         averageValues = np.mean(personTracks, axis = 0)
         trackMeans['keyTrack'] = averageValues
+
+    return trackMeans
+
+
+trackMeans = convert(values)
+danger = []
+urgency = []
+rof = []
+collab = []
+approach = []
+
+for trackKey, tv in trackMeans:
+    danger.append(tv[0])
+    urgency.append(tv[1])
+    rof.append(tv[2])
+    collab.append(tv[3])
+    approach.append(tv[4])
+
+plt.boxplot([danger, urgency, rof, collab, approach])
+plt.xticks([1, 2, 3, 4, 5], ['Danger', 'Urgency', 'Risk of Failure', 'Collaboration', 'Approachability'])
+
+plt.show()
